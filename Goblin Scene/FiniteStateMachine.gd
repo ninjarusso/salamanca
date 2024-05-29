986eebf0abaf02c3531753b647b14777af3e4545
Matisenitzky <matisenitzky@gmail.com>
2024-05-29 13:30:04 -0300

    Goblin Scene

---
+++ Goblin Scene/FiniteStateMachine.gd
@@ -0,0 +1,15 @@
+class_name FiniteStateMachine
+extends Node
+
+@export var state: State
+
+func _ready():
+	change_state(state)
+
+@onready var ray_cast_2d = $RayCast2D
+
+func change_state(new_state: State):
+	if state is State:
+		state._exit_state()
+	new_state._enter_state()
+	state = new_state
